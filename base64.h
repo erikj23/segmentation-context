97ba7e8bdcf4c3f550690148298455ed9184eaf0
erik maldonado <erikj23.em@gmail.com>
2019-11-13 13:39:03 -0800

    added base64 implementation and .h file partial implementation for conversion added test segments

---
+++ base64.h
@@ -0,0 +1,12 @@
+
+// Author: Erik Maldonado
+
+#pragma once
+
+// std
+#include <string>
+
+static const unsigned char base64_table[65] =
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+std::string base64_encode(const unsigned char* src, size_t len);
--- segmentation-context.cpp
+++ segmentation-context.cpp
@@ -1,20 +1,28 @@
 
-// Windows
-#include <tchar.h>
-#include <windows.h>
-#include <windows.data.json.h>
-
-// OpenCV
+// opencv
 #include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 
 // std
-#include <stdio.h>
-#include <string>
-#include <iostream>
+#include <cstdio>
 #include <filesystem>
+#include <iostream>
+#include <string>
+
+// custom
+#include "base64.h"
 
 // using
+using namespace cv;
 using namespace std;
+
+// constants
+const string EXTENSION = ".jpg";
+
+const filesystem::path INPUT_PATH("segments");
+const filesystem::path OUTPUT_PATH("output");
+const filesystem::path SEGMENT_PATH("segments");
 
 // to be implemented
 void generate_json();
@@ -22,18 +30,26 @@ void generate_bat();
 
 // work in progress
 // trying to get recursive file reader up
-void convert_segments(const string path)
+void convert_segments(vector<string>& encodings, const filesystem::path path)
 {
-	for (const auto& dirEntry : filesystem::recursive_directory_iterator(path))
-		if (!dirEntry.is_directory()) cout << dirEntry.path() << endl;
-	{	// now add code that converts files found to base64 strings
-		// then generate json
-		// then generate bats
-	}
+	Mat image;
+	vector<uchar> buffer;
+	for (const auto& entry : filesystem::recursive_directory_iterator(path))
+		if (entry.is_regular_file())
+		{
+			image = imread(entry.path().string());
+			imencode(EXTENSION, image, buffer);
+			auto *data = reinterpret_cast<unsigned char*>(buffer.data());
+			encodings.push_back(base64_encode(data, buffer.size()));
+			// then generate json			
+			// then generate bats
+		}
 }
 
 int main(int argc, char** argv)
 {	
-	convert_segments("segments");
+	// contains base64 encoded strings of images in the segments folder
+	vector<string> encodings;
+	convert_segments(encodings, SEGMENT_PATH);
 	return 0;
 }
