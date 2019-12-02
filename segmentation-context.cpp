73677699eb47ed44ebd1d5c75cd690d9ad5f7b2a
erik maldonado <erikj23.em@gmail.com>
2019-12-01 19:18:21 -0800

    finishing touches before merge

--- segmentation-context.cpp
+++ segmentation-context.cpp
@@ -45,37 +45,44 @@ const filesystem::path SEGMENT_PATH("segments");
 // global variables
 wstring_convert<codecvt_utf8_utf16<wchar_t>> converter;
 
-// assuptions:
-// outcome:
+// outcome: returns string converted from wstring
 string to_string(const wstring wide_string)
 {
 	return converter.to_bytes(wide_string);
 }
 
-// assuptions:
-// outcome:
+// outcome: returns wstring converted from string
 wstring to_wstring(const string normal_string)
 {
 	return converter.from_bytes(normal_string);
 }
 
 // assuptions:
-// outcome:
+//	file_name: is a valid file in the working directory that contains a
+//		working gcp api key for the vision api
+// outcome: api_key is stores a valid gcp api key
 void load_key(const string& file_name, string& api_key)
 {
 	ifstream key_file(file_name);
-	if (!getline(key_file, api_key)) printf("load key failure\n");
+
+	if (!getline(key_file, api_key))
+	{
+		printf("load key failure\n");
+		exit(EXIT_FAILURE);
+	}
 }
 
-// work in progress
 // assuptions:
+//	json_objects: properly initialized vector containing valid json::value
+//		objects that conform to the vision api request json schema 
+//	api_key: string that contains valid gcp vision api key
+//	group: properly initialized set 
 // outcome:
-set<string> make_requests(const vector<json::value>& json_objects, const string& api_key)
+//	group will be populated with all the label annotations associated with
+//		with the api responses
+void make_requests(const vector<json::value>& json_objects, const string& api_key, set<string>& group)
 {	
-	// set for json response collection
-	set<string> group;
-
-	if (json_objects.empty()) return group;
+	if (json_objects.empty()) return;
 
 	string label;
 
@@ -105,7 +112,6 @@ set<string> make_requests(const vector<json::value>& json_objects, const string&
 				label = to_string(object[L"description"].serialize());
 				label.erase(remove(label.begin(), label.end(), '\"'), label.end());
 				group.insert(label);
-			
 			}
 		});
 
@@ -113,11 +119,12 @@ set<string> make_requests(const vector<json::value>& json_objects, const string&
 		try { async_chain.wait(); }
 		catch (const exception & e) { printf("request exception:%s\n", e.what()); }
 	}
-	return group;
 }
 
 // assumptions:
-// outcome:
+//	encodings: properly intialized vector of base64 encoded images
+//	json_objects: properly intialized vector
+// outcome: valid vision api requests created and stored in json_objects
 // improvements:
 //	trade constants for variables
 //	more resilient failure handling
@@ -152,11 +159,14 @@ void generate_json(const vector<string>& encodings, vector<json::value>& json_ob
 }
 
 // assumptions:
-// outcome:
+//	path: valid path in the working directory that contains jpeg images grouped by directories
+//	api_key: valid gcp vision api key
+//	directory_labels: properly initialized map
+// outcome: images in path are labeled and stored in directory_labels
 // improvements:
 //	trade constant for variable
 //	more resilient failure handling
-void label_images(const filesystem::path& path, const string& api_key, map<string, set<string>>& folder_labels)
+void label_images(const filesystem::path& path, const string& api_key, map<string, set<string>>& directory_labels)
 {
 	const string EXTENSION = ".jpg";
 
@@ -168,7 +178,7 @@ void label_images(const filesystem::path& path, const string& api_key, map<strin
 	
 	// get first folder
 	auto directory = filesystem::recursive_directory_iterator(path);
-	string last_folder = directory->path().filename().string();
+	string last_directory = directory->path().filename().string();
 	directory.increment(e);
 
 	// convert each jpeg image found in path to base64
@@ -179,19 +189,18 @@ void label_images(const filesystem::path& path, const string& api_key, map<strin
 			generate_json(encodings, json_objects);
 			encodings.clear();
 
-			folder_labels[last_folder] = make_requests(json_objects, api_key);
+			directory_labels[last_directory] = set<string>();
+			make_requests(json_objects, api_key, directory_labels[last_directory]);
 			json_objects.clear();
 
-			last_folder = entry.path().filename().string();
+			last_directory = entry.path().filename().string();
 		}
 		if (entry.is_regular_file())
 		{	
 			image = imread(entry.path().string());
-			buffer.resize(static_cast<size_t>(image.rows)* static_cast<size_t>(image.cols));
+			buffer.resize(static_cast<size_t>(image.rows) * static_cast<size_t>(image.cols));
 
-			if (imencode(EXTENSION, image, buffer))
-				encodings.push_back(base64_encode(buffer.data(), buffer.size()));
-
+			if (imencode(EXTENSION, image, buffer)) encodings.push_back(base64_encode(buffer.data(), buffer.size()));
 			else printf("conversion failure\n");
 		}
 	}
@@ -202,49 +211,55 @@ void label_images(const filesystem::path& path, const string& api_key, map<strin
 		generate_json(encodings, json_objects);
 		encodings.clear();
 
-		folder_labels[last_folder] = make_requests(json_objects, api_key);
+		directory_labels[last_directory] = set<string>();
+		make_requests(json_objects, api_key, directory_labels[last_directory]);
 		json_objects.clear();
 	}
 }
 
-void write_json(const filesystem::path& path, const map<string, set<string>>& folder_labels, const string name)
+// assumptions:
+//	path: valid path in working directory
+//	directory_labels: properly initialized map that contains string to set mappings of an image
+//	name: non-empty string
+// outcome: directory_label mappings written to disk specified by path
+void write_json(const filesystem::path& path, const map<string, set<string>>& directory_labels, const string name)
 {	
 	int index;
 	json::value data;
+
 	// each iteration of key is one json object to written to disk
-	for (const auto& [key, value]: folder_labels)
+	for (const auto& [key, value]: directory_labels)
 	{	
-		//cout << "key: " << key << endl;
 		data = json::value::object();
 		data[to_wstring(key)] = json::value::array();
 		
 		index = 0;
 
-		for (const auto& label: value)
-		{	// put all values into json object
-			//cout << "val: " << label << endl;
-			data[to_wstring(key)][index++] = json::value(to_wstring(label));
-		}
+		for (const auto& label: value) data[to_wstring(key)][index++] = json::value(to_wstring(label));
 
-		// create directory with this key name, fails if already exists
-		//cout << to_string(data.serialize()) << endl;
+		// create directory with this key name, fails if already exists		
 		filesystem::create_directory(path.string() + "\\" + key);
 
+		// write file to directory
 		ofstream json_file(path.string() + "\\" + key + "\\" + name + ".json");
 		json_file << to_string(data.serialize());
 		json_file.close();
 	}
 }
-//
+
+// outcomes: 
+//	loads api key from disk
+//	labels all images in segments directory
+//	writes labels as json files to disk in output directory
 int main(int argc, char** argv)
 {	
 	string api_key;
-	map<string, set<string>> folder_labels;
+	map<string, set<string>> directory_labels;
 
 	//validate(argv) add error handling for arguments
 	load_key(argv[1], api_key);
-	label_images(SEGMENT_PATH, api_key, folder_labels);
-	write_json(OUTPUT_PATH, folder_labels, "segments");
+	label_images(SEGMENT_PATH, api_key, directory_labels);
+	write_json(OUTPUT_PATH, directory_labels, "segments");
 
 	return EXIT_SUCCESS;
 }
