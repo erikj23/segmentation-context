c411155a6a8ff10f90ba0326ffbeaa2295a9fe42
Unknown <cd47@uw.edu>
2019-12-02 01:34:01 -0800

    comments and some merge error

--- kmean-segmentation.cpp
+++ kmean-segmentation.cpp
@@ -1,21 +1,39 @@
 
+// 11 2019, Authors:
+//	Jeremy Deng
+//	Erik Maldonado
+
+//opencv
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+
+//std
 #include <cmath>
-#include <string>
+#include <String>
 #include <iostream>
 #include <direct.h> 
+
+//namespace
 using namespace cv;
+using namespace std;
 
 
-void k_mean (String filename, int cluster_size) {
-	Mat ocv = imread(filename);
-	String file_dir = filename.substr(8, filename.substr(8).find("."));
-	String create_dir = format("./Segmentation/%s", file_dir.c_str());
+// assumptions:
+//	filename: valid image file in working directory
+//	cluster_size: integer values: [2-20]
+// outcome: outputing the image into segments 
+void segmentation (const string& filename,const& int cluster_size) {
+	//read in the image 
+	Mat input = imread(filename);
+	Mat ocv = input.clone();
+
+	//making output folder
+	string file_dir = filename.substr(9, filename.substr(9).find("."));
+	string create_dir = format("./Segments/%s", file_dir.c_str());
 	_mkdir(create_dir.c_str());
 
-	// convert to float & reshape to a [3 x W*H] Mat 
+	// convert image pixel to float & reshape to a [3 x W*H] Mat 
 	//  (so every pixel is on a row of it's own)
 	Mat data;
 	ocv.convertTo(data, CV_32F);
@@ -27,10 +45,11 @@ void k_mean (String filename, int cluster_size) {
 	kmeans(data, clusters, labels, TermCriteria(TermCriteria::EPS + TermCriteria::MAX_ITER, 10, 1.0), 10,
 		KMEANS_RANDOM_CENTERS, centers);
 
-	// reshape both to a single row of Vec3f pixels:
+	// reshape labels and cluster centers to a single row of Vec3f pixels:
 	centers = centers.reshape(3, centers.rows);
 	data = data.reshape(3, data.rows);
 
+	//for each cluster, outputing the segments 
 	for (int center_id = 0; center_id < clusters; center_id++) {
 		Mat temp = data.clone();
 		Vec3f* pointer = temp.ptr<Vec3f>();
@@ -42,13 +61,12 @@ void k_mean (String filename, int cluster_size) {
 				pointer[i] = Vec3f{ 0,0,0 };
 			}
 		}
-		String output_dir = format("./Segmentation/%s/%s_%d.jpg", file_dir.c_str(), file_dir.c_str(), center_id);
+		String output_dir = format("./Segments/%s/%s_%d.jpg", file_dir.c_str(), file_dir.c_str(), center_id);
 		temp = temp.reshape(3, ocv.rows);
 		temp.convertTo(temp, CV_8U);
 		imwrite(output_dir, temp);	
 	}
 	
-
 	// replace pixel values with their center value:
 	Vec3f* p = data.ptr<Vec3f>();
 	for (int i = 0; i < data.rows; i++) {
@@ -60,17 +78,18 @@ void k_mean (String filename, int cluster_size) {
 	ocv = data.reshape(3, ocv.rows);
 	ocv.convertTo(ocv, CV_8U);
 
+	//display the k mean result and output into the segment directory
 	namedWindow("Original Image");
 	imshow("Original Image", ocv);
 	waitKey(0);
-	imwrite(format("./Segmentation/%s/%s_full.jpg", file_dir.c_str(), file_dir.c_str()), ocv);
+	imwrite(format("./output/%s/%s_full.jpg", file_dir.c_str(), file_dir.c_str()), ocv);
 }
 
 
 int main(int argc, char* argv[])
 {
-	String filename = "./input/background.jpg";
-	k_mean (filename, 8);
+	String filename = "./images/background.jpg";
+	segmentation (filename, 8);
 
 	return 0;
 }
